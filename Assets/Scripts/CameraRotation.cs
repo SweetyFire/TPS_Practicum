9cc637df577f75d53c6f5c2d931bed60b62ca1a8
Miyphario <159008869+SweetyFire@users.noreply.github.com>
2024-02-07 12:49:49 +0600

    Camera rotation

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    [SerializeField] private Transform _cameraAxisTransform;
+    [SerializeField] private float _rotationSpeed = 2f;
+
+    [SerializeField] private float _minAngle;
+    [SerializeField] private float _maxAngle;
+
+    private void Update()
+    {
+        float rotY = transform.localEulerAngles.y + _rotationSpeed * Time.deltaTime * Input.GetAxis("Mouse X");
+        transform.localEulerAngles = new(0, rotY, 0);
+
+        float rotX = _cameraAxisTransform.localEulerAngles.x - _rotationSpeed * Time.deltaTime * Input.GetAxis("Mouse Y");
+        if (rotX > 180)
+            rotX -= 360;
+
+        rotX = Mathf.Clamp(rotX, _minAngle, _maxAngle);
+        _cameraAxisTransform.localEulerAngles = new(rotX, 0, 0);
+    }
+}
